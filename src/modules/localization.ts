ffe946ba11f528c2dfc04fbfffdab5cdd0a9ea36
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2024-09-18 13:16:43 +0300

    Fix localization in packaged GoofCord

--- src/modules/localization.ts
+++ src/modules/localization.ts
@@ -1,9 +1,19 @@
 import fs from "node:fs";
 import path from "node:path";
+import { ipcMain, ipcRenderer } from "electron";
 
 const lang = "en-US";
-const localization = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "assets", "lang", lang + ".json"), "utf-8"));
-const defaultLang = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "assets", "lang", "en-US.json"), "utf-8"));
+let localization: object;
+let defaultLang: object;
+if (process.type === "browser") {
+	localization = JSON.parse(fs.readFileSync(path.join(__dirname, "assets", "lang", lang + ".json"), "utf-8"));
+	defaultLang = JSON.parse(fs.readFileSync(path.join(__dirname, "assets", "lang", "en-US.json"), "utf-8"));
+	ipcMain.on("localization:getObjects", (event) => {
+		event.returnValue = [localization, defaultLang];
+	});
+} else {
+	[localization, defaultLang] = ipcRenderer.sendSync("localization:getObjects");
+}
 
 // Gets localized string. Shortened because it's used very often
 export function i(key: string) {
