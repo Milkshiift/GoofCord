b537d5ef5eaf516aaf28222125525fc1ce54bd27
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-02-04 14:15:20 +0200

    Ask for confirmation to accept invite

--- src/modules/arrpc.ts
+++ src/modules/arrpc.ts
@@ -1,5 +1,7 @@
+import { dialog } from "electron";
 import { getConfig } from "../config.ts";
 import { mainWindow } from "../windows/main/main.ts";
+import { i } from "./localization.ts";
 
 export async function initArrpc() {
 	if (!getConfig("arrpc")) return;
@@ -8,13 +10,21 @@ export async function initArrpc() {
 		const Bridge = await import("arrpc/src/bridge.js");
 		const server = new Server();
 		server.on("activity", (data: object) => Bridge.send(data));
-		server.on("invite", (code: string) => {
-			mainWindow.webContents.executeJavaScript(`
-				shelter.http.post({
-  					url: "/invites/${code}"
-				})
-			`);
-			mainWindow.show();
+		server.on("invite", async (code: string) => {
+			const inviteRequest = await fetch("https://discordapp.com/api/v9/invites/" + code);
+			const inviteData = await inviteRequest.json();
+			const response = await dialog.showMessageBox({
+				message: i("inviteMessage")+" "+inviteData.guild.name+"?",
+				buttons: [i("yes"), i("no")],
+			});
+			if (response.response === 0) {
+				void mainWindow.webContents.executeJavaScript(`
+					shelter.http.post({
+						url: "/invites/${code}"
+					})
+				`);
+				mainWindow.show();
+			}
 		});
 	} catch (e) {
 		console.error("Failed to start arRPC server", e);
