feb47831e95fedcb79894ba339a0b39c683ca710
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-02-17 20:19:40 +0200

    Use shelteRPC for invites instead of a dialog

--- src/modules/arrpc.ts
+++ src/modules/arrpc.ts
@@ -1,8 +1,6 @@
-import { dialog } from "electron";
 import { getConfig } from "../config.ts";
 import { getGoofCordFolderPath } from "../utils.ts";
 import { mainWindow } from "../windows/main/main.ts";
-import { i } from "./localization.ts";
 
 export async function initArrpc() {
 	if (!getConfig("arrpc")) return;
@@ -11,22 +9,32 @@ export async function initArrpc() {
 		const Bridge = await import("arrpc/src/bridge.js");
 		const server = new Server(getGoofCordFolderPath() + "/detectable.json");
 		server.on("activity", (data: object) => Bridge.send(data));
-		server.on("invite", async (code: string) => {
-			const inviteRequest = await fetch("https://discordapp.com/api/v9/invites/" + code);
-			const inviteData = await inviteRequest.json();
-			const response = await dialog.showMessageBox({
-				message: i("inviteMessage")+" "+inviteData.guild.name+"?",
-				buttons: [i("yes"), i("no")],
-			});
-			if (response.response === 0) {
-				void mainWindow.webContents.executeJavaScript(`
-					shelter.http.post({
-						url: "/invites/${code}"
-					})
-				`);
-				mainWindow.show();
-			}
+		server.on("invite", (code: string) => {
+			mainWindow.show();
+			Bridge.send({
+				cmd: "INVITE_BROWSER",
+				args: {
+					"code": code
+				}
+			})
 		});
+
+		// server.on("invite", async (code: string) => {
+		// 	const inviteRequest = await fetch("https://discordapp.com/api/v9/invites/" + code);
+		// 	const inviteData = await inviteRequest.json();
+		// 	const response = await dialog.showMessageBox({
+		// 		message: i("inviteMessage")+" "+inviteData.guild.name+"?",
+		// 		buttons: [i("yes"), i("no")],
+		// 	});
+		// 	if (response.response === 0) {
+		// 		void mainWindow.webContents.executeJavaScript(`
+		// 			shelter.http.post({
+		// 				url: "/invites/${code}"
+		// 			})
+		// 		`);
+		// 		mainWindow.show();
+		// 	}
+		// });
 	} catch (e) {
 		console.error("Failed to start arRPC server", e);
 	}
