1dd3ebd44932938a24007cd90b38d0e632038182
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-03-16 19:44:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,34 +14,42 @@ setFlags();
 if (!app.requestSingleInstanceLock()) app.exit();
 
 async function main() {
-	console.time(pc.green("[Timer]") + " GoofCord fully loaded in");
+    console.time(pc.green("[Timer]") + " GoofCord fully loaded in");
 
-	await loadConfig();
-	await initLocalization();
+    await loadConfig();
+    await initLocalization();
 
-	const loader = await import("./loader");
-	await loader.load();
+    const loader = await import("./loader");
+    await loader.load();
 }
 
 function setFlags() {
-	if (process.argv.some((arg) => arg === "--no-flags")) return;
-	const disableFeatures = ["OutOfBlinkCors", "UseChromeOSDirectVideoDecoder", "HardwareMediaKeyHandling", "MediaSessionService", "WebRtcAllowInputVolumeAdjustment", "Vulkan"];
-	const enableFeatures = [
-		"WebRTC",
-		"WebRtcHideLocalIpsWithMdns",
-		"PlatformHEVCEncoderSupport",
-		"EnableDrDc",
-		"CanvasOopRasterization",
-		"UseSkiaRenderer",
-		"AcceleratedVideoEncoder",
-		"AcceleratedVideoDecoder",
-	];
-	if (process.platform === "linux") enableFeatures.push("PulseaudioLoopbackForScreenShare", "VaapiVideoDecoder", "VaapiVideoEncoder", "VaapiVideoDecodeLinuxGL", "AcceleratedVideoDecodeLinuxGL", "AcceleratedVideoDecodeLinuxZeroCopyGL");
-	app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
-	app.commandLine.appendSwitch("enable-speech-dispatcher");
-	app.commandLine.appendSwitch("disable-features", disableFeatures.join(","));
-	app.commandLine.appendSwitch("enable-features", enableFeatures.join(","));
-	app.commandLine.appendSwitch("disable-http-cache");
+    if (process.argv.some((arg) => arg === "--no-flags")) return;
+
+    const disableFeatures = [
+        "OutOfBlinkCors",
+        "UseChromeOSDirectVideoDecoder",
+        "HardwareMediaKeyHandling",
+        "MediaSessionService",
+        "WebRtcAllowInputVolumeAdjustment",
+        "Vulkan"
+    ];
+    const enableFeatures = [
+        "WebRTC",
+        "WebRtcHideLocalIpsWithMdns",
+        "PlatformHEVCEncoderSupport",
+        "EnableDrDc",
+        "CanvasOopRasterization",
+        "UseSkiaRenderer"
+    ];
+    if (process.platform === "linux") enableFeatures.push("PulseaudioLoopbackForScreenShare");
+    if (!process.argv.some((arg) => arg === "--no-vaapi")) enableFeatures.push("AcceleratedVideoDecodeLinuxGL", "AcceleratedVideoEncoder", "AcceleratedVideoDecoder", "AcceleratedVideoDecodeLinuxZeroCopyGL");
+
+    app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
+    app.commandLine.appendSwitch("enable-speech-dispatcher");
+    app.commandLine.appendSwitch("disable-features", disableFeatures.join(","));
+    app.commandLine.appendSwitch("enable-features", enableFeatures.join(","));
+    app.commandLine.appendSwitch("disable-http-cache");
 }
 
 main().catch(console.error);
