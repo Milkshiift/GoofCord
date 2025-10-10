c484e180116e94a3424484b21b6f2035d46bb67b
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-10-10 10:58:57 +0300

    Use scrypt for key derivation for cloud storage

--- src/windows/settings/cloud/encryption.ts
+++ src/windows/settings/cloud/encryption.ts
@@ -4,6 +4,7 @@ import zlib from "node:zlib";
 
 const brotliCompress = util.promisify(zlib.brotliCompress);
 const brotliDecompress = util.promisify(zlib.brotliDecompress);
+const scrypt = util.promisify(crypto.scrypt);
 
 const SALT_LENGTH = 32;
 const IV_LENGTH = 12; // 96 bits as per NIST recommendations (SP 800-38D): https://csrc.nist.gov/pubs/sp/800/38/d/final
@@ -11,10 +12,11 @@ const TAG_LENGTH = 16;
 const KEY_LENGTH = 32;
 
 // As per OWASP recommendations: https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
-const ARGON2_ALGORITHM = "argon2id";
-const ARGON2_PASSES = 2;
-const ARGON2_PARALLELISM = 1;
-const ARGON2_MEMORY_COST = 19 * 1024; // 19 MiB in KiB (19 * 1024)
+// N=2^15 (32 MiB), r=8 (1024 bytes), p=3
+const SCRYPT_COST = 32768;
+const SCRYPT_BLOCK_SIZE = 8;
+const SCRYPT_PARALLELIZATION = 3;
+const SCRYPT_MAX_MEM = 128 * SCRYPT_COST * SCRYPT_BLOCK_SIZE * 2;
 
 export async function encryptString(text: string, password: string): Promise<string | undefined> {
 	try {
@@ -26,14 +28,13 @@ export async function encryptString(text: string, password: string): Promise<str
 		}
 
 		const salt = crypto.randomBytes(SALT_LENGTH);
-		const key = crypto.argon2Sync(ARGON2_ALGORITHM, {
-			message: Buffer.from(password),
-			nonce: salt,
-			parallelism: ARGON2_PARALLELISM,
-			tagLength: KEY_LENGTH,
-			memory: ARGON2_MEMORY_COST,
-			passes: ARGON2_PASSES,
-		});
+		// @ts-expect-error
+		const key = (await scrypt(password, salt, KEY_LENGTH, {
+			N: SCRYPT_COST,
+			r: SCRYPT_BLOCK_SIZE,
+			p: SCRYPT_PARALLELIZATION,
+			maxmem: SCRYPT_MAX_MEM,
+		})) as Buffer;
 
 		const iv = crypto.randomBytes(IV_LENGTH);
 		const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
@@ -67,14 +68,13 @@ export async function decryptString(encryptedText: string, password: string): Pr
 		const authTag = buffer.subarray(SALT_LENGTH + IV_LENGTH, SALT_LENGTH + IV_LENGTH + TAG_LENGTH);
 		const encrypted = buffer.subarray(SALT_LENGTH + IV_LENGTH + TAG_LENGTH);
 
-		const key = crypto.argon2Sync(ARGON2_ALGORITHM, {
-			message: Buffer.from(password),
-			nonce: salt,
-			parallelism: ARGON2_PARALLELISM,
-			tagLength: KEY_LENGTH,
-			memory: ARGON2_MEMORY_COST,
-			passes: ARGON2_PASSES,
-		});
+		// @ts-expect-error
+		const key = (await scrypt(password, salt, KEY_LENGTH, {
+			N: SCRYPT_COST,
+			r: SCRYPT_BLOCK_SIZE,
+			p: SCRYPT_PARALLELIZATION,
+			maxmem: SCRYPT_MAX_MEM,
+		})) as Buffer;
 
 		const decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
 		decipher.setAuthTag(authTag);
