693414730d0e102e30f9b3fd92f30a81ef05258d
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2026-03-06 22:35:13 +0200

    Fix invidious embeds

--- assets/preVencord.js
+++ assets/preVencord.js
@@ -65,7 +65,7 @@ var devtoolsFix_default = definePatch({
 var invidiousEmbeds_default = definePatch({
   patches: [
     {
-      find: ',"%"),maxWidth',
+      find: 'analyticsSource:"EmbedVideo"',
       replacement: {
         match: /(:[^,]+,src:[^\.]+.url)/,
         replace: "$1?.replace('https://www.youtube.com/embed/', (window.invidiousInstance ?? 'https://www.youtube.com')+'/embed/')+'?autoplay=1&player_style=youtube&local=true'"
--- src/windows/main/renderer/preVencord/patches/invidiousEmbeds.ts
+++ src/windows/main/renderer/preVencord/patches/invidiousEmbeds.ts
@@ -3,7 +3,7 @@ import { definePatch } from "../patchManager.ts";
 export default definePatch({
 	patches: [
 		{
-			find: ',"%"),maxWidth',
+			find: 'analyticsSource:"EmbedVideo"',
 			replacement: {
 				match: /(:[^,]+,src:[^\.]+.url)/,
 				replace: "$1?.replace('https://www.youtube.com/embed/', (window.invidiousInstance ?? 'https://www.youtube.com')+'/embed/')+'?autoplay=1&player_style=youtube&local=true'",
